6cdb137e49d541bb991b46692feddf3a3f07b2a5
Alfredo Rafael Pabon <alfredrafael@gmail.com>
2023-04-08 20:34:46 -0500

    Integrate TaxData with ReverseGeoCode

--- src/App.tsx
+++ src/App.tsx
@@ -42,6 +42,7 @@ function useUserLocation() {
 
 function ReverseGeocode(props: any) {
   const [zipcode, setZipcode] = useState(null);
+  const [taxData, setTaxData] = useState<any>([]);
 
   useEffect(() => {
     const { latitude, longitude } = props;
@@ -57,20 +58,10 @@ function ReverseGeocode(props: any) {
         setZipcode(result ? result.address_components[0].short_name : null);
       })
       .catch((error) => console.error(error));
-  }, [props.latitude, props.longitude]);
 
-  return <div>{zipcode ? <p>Zipcode: {zipcode}</p> : <p>Loading...</p>}</div>;
-}
+    //  ------------------------------ //
 
-{
-  /* // Build Tax URL////////////////////////////////////////////////////////////////////// */
-}
-
-function TaxData() {
-  const [taxData, setTaxData] = useState<any>([]);
-
-  useEffect(() => {
-    const taxAPIUrl = `https://u-s-a-sales-taxes-per-zip-code.p.rapidapi.com/${"02115"}`;
+    const taxAPIUrl = `https://u-s-a-sales-taxes-per-zip-code.p.rapidapi.com/${zipcode}`;
 
     // TaxAPI headers
     const taxAPIOptions = {
@@ -89,17 +80,57 @@ function TaxData() {
         setTaxData([data]);
       })
       .catch((err) => console.error(err));
-  }, []);
+  }, [props.latitude, props.longitude]);
 
   return (
-    // <div>{taxData ? <p>{JSON.stringify(taxData[0])}</p> : "No tax data"} </div>
     <div>
+      {zipcode ? <p>Zipcode: {zipcode}</p> : <p>Loading...</p>}{" "}
       {taxData
-        ? taxData.map((item: any, i: any) => <p>{item.state}</p>)
+        ? taxData.map((item: any, i: any) => (
+            <p>{item ? item.state : "No state"}</p>
+          ))
         : "No tax data"}{" "}
     </div>
   );
 }
+
+{
+  /* // Build Tax URL////////////////////////////////////////////////////////////////////// */
+}
+
+// function TaxData() {
+//   const [taxData, setTaxData] = useState<any>([]);
+
+//   useEffect(() => {
+//     const taxAPIUrl = `https://u-s-a-sales-taxes-per-zip-code.p.rapidapi.com/${"02115"}`;
+
+//     // TaxAPI headers
+//     const taxAPIOptions = {
+//       method: "GET",
+//       headers: {
+//         "X-RapidAPI-Key": "ccdfef5bcfmsh039706eb6485b4dp119607jsnaf780f8fff24",
+//         "X-RapidAPI-Host": "u-s-a-sales-taxes-per-zip-code.p.rapidapi.com",
+//       },
+//     };
+
+//     // Fetch taxData
+//     fetch(taxAPIUrl, taxAPIOptions)
+//       .then((response) => response.json())
+//       .then((data: []) => {
+//         console.log("TaxData: ", data);
+//         setTaxData([data]);
+//       })
+//       .catch((err) => console.error(err));
+//   }, []);
+
+//   return (
+//     <div>
+//       {taxData
+//         ? taxData.map((item: any, i: any) => <p>{item.state}</p>)
+//         : "No tax data"}{" "}
+//     </div>
+//   );
+// }
 
 {
   /* // Display ////////////////////////////////////////////////////////////////////// */
@@ -123,7 +154,6 @@ function App() {
       {latitude && longitude && (
         <ReverseGeocode latitude={latitude} longitude={longitude} />
       )}
-      <TaxData />
     </div>
   );
 }
