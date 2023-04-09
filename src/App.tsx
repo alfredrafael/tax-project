dc995fa64b2d60b16a8403de01a3f92f0f1242e3
Alfredo Rafael Pabon <alfredrafael@gmail.com>
2023-04-09 08:40:12 -0500

    Change title

--- src/App.tsx
+++ src/App.tsx
@@ -112,11 +112,12 @@ function App() {
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
+
         <a href="https://reactjs.org" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Vite & React Tax Disclosure</h1>
       {/* Your coordiantates: {useUserLocation()} */}
       {latitude && longitude && (
         <ReverseGeocode latitude={latitude} longitude={longitude} />
