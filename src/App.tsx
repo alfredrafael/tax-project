8f6a559e97b7af98ed77d18b2ad5028b5d73ad41
Alfredo Rafael Pabon <alfredrafael@gmail.com>
2023-04-10 00:04:29 -0500

    Move variable to .env file

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,9 @@ import "./App.css";
 import axios from "axios";
 
 const GOOGLE_API_KEY = import.meta.env.VITE_GOOGLE_API_KEY;
+const TAX_API_KEY = import.meta.env.VITE_TAX_API_KEY;
 
-{
-  /* // Get Coordinates ////////////////////////////////////////////////////////////////////// */
-}
+/* // Get Coordinates ////////////////////////////////////////////////////////////////////// */
 
 function useUserLocation() {
   const [latitude, setLatitude] = useState<number>();
@@ -36,9 +35,8 @@ function useUserLocation() {
 
   return [latitude, longitude];
 }
-{
-  /* // Get Zip Code////////////////////////////////////////////////////////////////////// */
-}
+
+/* // Get Zip Code////////////////////////////////////////////////////////////////////// */
 
 function ReverseGeocode(props: any) {
   const [zipcode, setZipcode] = useState<any>();
@@ -67,8 +65,7 @@ function ReverseGeocode(props: any) {
       // TaxAPI headers
       const taxAPIOptions = {
         headers: {
-          "X-RapidAPI-Key":
-            "ccdfef5bcfmsh039706eb6485b4dp119607jsnaf780f8fff24",
+          "X-RapidAPI-Key": `${TAX_API_KEY}`,
           "X-RapidAPI-Host": "u-s-a-sales-taxes-per-zip-code.p.rapidapi.com",
         },
       };
@@ -89,15 +86,75 @@ function ReverseGeocode(props: any) {
 
   return (
     <div>
-      {zipcode ? <p>Your Zipcode: {zipcode}</p> : <p></p>}{" "}
+      {zipcode ? <p>Your zipcode: {zipcode}</p> : <p></p>}{" "}
       <p>Your State: {taxData.state}</p>
       <p>Your State rate: {taxData.state_rate}</p>
-      <p>Your Estimated City Rate: {taxData.estimated_city_rate}</p>
-      <p>Your Estimated County rate: {taxData.estimated_county_rate}</p>
-      <p>Your Estimated Combined Rate: {taxData.estimated_combined_rate}</p>
+      <p>Your estimated city Rate: {taxData.estimated_city_rate}</p>
+      <p>Your estimated county rate: {taxData.estimated_county_rate}</p>
+      <p>Your estimated combined Rate: {taxData.estimated_combined_rate}</p>
     </div>
   );
 }
+
+// const ZipCodeInput = () => {
+//   const [inputValue, setInputValue] = useState<string>("");
+//   const [isValid, setIsValid] = useState<boolean>(true);
+//   const [taxData, setTaxData] = useState<any>([]);
+
+//   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+//     const newValue = event.target.value;
+//     setInputValue(newValue);
+//     // Check if the new value is a valid zip code
+//     const isValidZip = isValidZipCode(newValue);
+//     setIsValid(isValidZip);
+//   };
+
+//   const handleClick = () => {
+//     if (isValid) {
+//       useEffect(() => {
+//         const taxAPIUrl = `https://u-s-a-sales-taxes-per-zip-code.p.rapidapi.com/${inputValue}`;
+
+//         // TaxAPI headers
+//         const taxAPIOptions = {
+//           headers: {
+//             "X-RapidAPI-Key":
+//               "ccdfef5bcfmsh039706eb6485b4dp119607jsnaf780f8fff24",
+//             "X-RapidAPI-Host": "u-s-a-sales-taxes-per-zip-code.p.rapidapi.com",
+//           },
+//         };
+
+//         // Fetch taxData
+//         axios
+//           .get(taxAPIUrl, taxAPIOptions)
+//           .then((response) => {
+//             setTaxData(response.data);
+//           })
+//           .catch((error) => {
+//             console.error(error);
+//           });
+//       }, []);
+
+//       return <p>hello</p>;
+//     } else {
+//       alert("is not valid");
+//     }
+//   };
+
+//   return (
+//     <div>
+//       <input
+//         className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline max-w-[50%]"
+//         placeholder="Enter zipcode here"
+//         type="text"
+//         value={inputValue}
+//         onChange={handleChange}
+//       />
+//       <button onClick={handleClick}>Get tax data</button>
+//       {!isValid && <div>Please enter a valid zip code.</div>}
+
+//     </div>
+//   );
+// };
 
 {
   /* // Display ////////////////////////////////////////////////////////////////////// */
@@ -107,7 +164,7 @@ function App() {
   const [latitude, longitude] = useUserLocation();
 
   return (
-    <div>
+    <div className="">
       <div className="flex justify-between">
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -118,10 +175,11 @@ function App() {
         </a>
       </div>
       <h1>ReactJS Tax Data</h1>
-      {/* Your coordiantates: {useUserLocation()} */}
       {latitude && longitude && (
         <ReverseGeocode latitude={latitude} longitude={longitude} />
       )}
+      Your coordiantates: {useUserLocation()}
+      <div>{/* <ZipCodeInput /> */}</div>
     </div>
   );
 }
